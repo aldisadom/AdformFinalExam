2b0e0069952a20f5450104bf88bb2eab1595c4f2
Aldis Adomavičius <aldisadom@gmail.com>
2024-02-19 17:29:21 +0200

    ghost migration file removed

--- migrations/sql/007-Alter-orders-table-delete-cascade.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
--- changeset Aldis:6
--- comment: Create orders_items table
-ALTER TABLE orders
-ADD create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(0);
-
--- rollback ALTER TABLE orders DROP COLUMN create_date;;
